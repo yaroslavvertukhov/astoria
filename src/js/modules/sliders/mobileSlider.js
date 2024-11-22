6319157f36fd1bac49474ee67878988116032b57
v.varganov <antresolrec@yandex.ru>
2024-11-22 21:42:01 +0300

    Мероприятия. Детальная с картинкой и несколькими мероприятиями

--- src/js/modules/sliders/mobileSlider.js
+++ src/js/modules/sliders/mobileSlider.js
@@ -17,6 +17,7 @@ export default class MobileSlider {
 	}
 
 	init() {
+		this.checkCurMediaQuery();
 		this.bindMethods();
 		this.checkSlider();
 		this.addListenerResize();
@@ -28,6 +29,14 @@ export default class MobileSlider {
 
 	addListenerResize() {
 		this.mediaQuery.addEventListener('change', this.resizeHandler);
+	}
+
+	checkCurMediaQuery() {
+		if (this.container.hasAttribute('data-init')) {
+			const num = this.container.getAttribute('data-init');
+
+			this.mediaQuery = window.matchMedia(`(max-width: ${num}px)`);
+		}
 	}
 
 	checkSlider() {
